62e5435092aecca4c7c420dd19d78d8d6553bead
condor <support@unetway.com>
2023-06-10 18:16:59 +0600

    refactor code

--- src/MonthsDays.php
+++ src/MonthsDays.php
@@ -2,42 +2,25 @@
 
 namespace Unetway\MonthsDays;
 
+use DateTimeImmutable;
 use Exception;
 
 class MonthsDays
 {
-
     /**
      * @param int $day
-     * @return false|float
+     * @return int
      * @throws Exception
      */
-    public function getMonthByDays(int $day)
+    public function getMonthByDays(int $day): int
     {
-        if (empty($day)) {
+        if ($day <= 0) {
             throw new Exception('Day is not defined');
         }
 
         $daysInMonth = $this->getDaysInMonth();
-        $month = $day / $daysInMonth;
-        $month = floor($month);
 
-        return $month;
-    }
-
-    /**
-     * @param int $month
-     * @return float|int
-     * @throws Exception
-     */
-    public function getDaysByMonths(int $month)
-    {
-        if (empty($month)) {
-            throw new Exception('Month is not defined');
-        }
-
-        $daysInMonth = $this->getDaysInMonth();
-        return $month * $daysInMonth;
+        return (int)ceil($day / $daysInMonth);
     }
 
     /**
@@ -45,9 +28,25 @@ class MonthsDays
      */
     private function getDaysInMonth(): int
     {
-        $currentMonth = date('n');
-        $currentYear = date('Y');
-        return cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
+        $currentMonth = (int)(new DateTimeImmutable())->format('n');
+        $currentYear = (int)(new DateTimeImmutable())->format('Y');
+
+        return (int)cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
     }
 
+    /**
+     * @param int $month
+     * @return int
+     * @throws Exception
+     */
+    public function getDaysByMonths(int $month): int
+    {
+        if ($month <= 0) {
+            throw new Exception('Month is not defined');
+        }
+
+        $daysInMonth = $this->getDaysInMonth();
+
+        return $month * $daysInMonth;
+    }
 }
